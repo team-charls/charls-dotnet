027d0ed0447a62a3c4e9e82bf900bd7a90020f30
Victor Derks <vbaderks@gmail.com>
2025-03-15 22:06:13 +0100

    Convert .sln format to .slnx

--- src/JpegLSEncoder.cs
+++ src/JpegLSEncoder.cs
@@ -20,7 +20,6 @@ public sealed class JpegLSEncoder
 
     private JpegStreamWriter _writer;
     private ScanEncoder _scanEncoder;
-    private FrameInfo _frameInfo;
     private int _nearLossless;
     private InterleaveMode _interleaveMode;
     private ColorTransformation _colorTransformation;
@@ -92,16 +91,7 @@ public sealed class JpegLSEncoder
     /// </value>
     /// <exception cref="ArgumentException">Thrown when the passed FrameInfo is invalid.</exception>
     /// <exception cref="ArgumentNullException">Thrown when the passed FrameInfo instance is null.</exception>
-    public FrameInfo FrameInfo
-    {
-        get => _frameInfo;
-
-        set
-        {
-            ArgumentNullException.ThrowIfNull(value);
-            _frameInfo = value;
-        }
-    }
+    public FrameInfo FrameInfo { get; set; }
 
     /// <summary>
     /// Gets or sets the near lossless parameter to be used to encode the JPEG-LS stream.
